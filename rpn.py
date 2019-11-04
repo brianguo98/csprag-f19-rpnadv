c3292bc950b2365ec2bfc584afa0e18a0c370da3
Brian Guo <brianguo@0587414063.wireless.umich.net>
2019-11-04 11:58:50 -0500

    commented out test

--- rpn.py
+++ rpn.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import operator
-
+import readline
+from gtts import gTTS
+import os
+from termcolor import colored
 
 operators = {
     '+': operator.add,
@@ -10,8 +13,16 @@ operators = {
     '/': operator.truediv,
 }
 
+operator_names = {
+    '+': 'plus',
+    '-': 'minus',
+    '*': 'multiplied by',
+    '/': 'divided by',
+}
+
 def calculate(myarg):
     stack = list()
+    speak = ''
     for token in myarg.split():
         try:
             token = int(token)
@@ -22,15 +33,19 @@ def calculate(myarg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
+            speak = str(arg1) + operator_names[token] + str(arg2)
         print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
-    return stack.pop()
+    return stack.pop(), speak
 
 def main():
     while True:
-        result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        result, speak = calculate(input("rpn calc> "))
+        print(colored("Result: ", 'red'), colored(result, 'green'))
+        tts = gTTS(text=speak + 'is' + str(result), lang='en')
+        tts.save("good.mp3")
+        os.system("mpg321 -q good.mp3")
 
 if __name__ == '__main__':
     main()
